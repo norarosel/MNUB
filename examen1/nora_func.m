7ef1fa06b1205b2e03959e149981f8ee5ff8e50f
Nora Cécile Rosel Zaballos <133972724+norarosel@users.noreply.github.com>
2025-06-16 00:56:28 +0200

    Update nora_func.m

--- examen1/nora_func.m
+++ examen1/nora_func.m
@@ -1,4 +1,4 @@
-function [f] = Barato_Abad_func(x,y)
+function [f] = nora_func(x,y)
 %BARATO_ABAD_FUNC Summary of this function goes here
 %   Detailed explanation goes here
 if ((x.^2+y.^2)>= 0) && ((x.^2+y.^2)< 1)
